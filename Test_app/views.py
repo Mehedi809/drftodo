6dbabb2e61e81768bee6c6b6906e9d6172a5a751
Mehedi809 <mehedihhasan809.com>
2025-03-12 03:06:40 +0600

    second day

--- Test_app/views.py
+++ Test_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIVIew,
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    RetrieveDestroyAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+from rest_framework import viewsets
+from restframework.permissions import Allowonly
+from django.http import JsonResponse
+from .models import *
+from .serializers import *
 # Create your views here.
